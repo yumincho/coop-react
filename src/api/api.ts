822fece979f51b9b670458f3001ac442b7ef0e02
Yumin Cho <dbals081216@gmail.com>
2024-07-21 22:32:40 +0900

    :sparkles: Add API functions for CRUD on todos

---
+++ src/api/api.ts
@@ -0,0 +1,93 @@
+import Pocketbase from 'pocketbase'
+
+import { Todo } from '../store/todoStore'
+
+const pb = new Pocketbase(import.meta.env.VITE_API_BASE)
+
+const TODOS = 'todos'
+
+export const createTodo = async () => {
+  try {
+    const res = await pb.collection(TODOS).create({
+      completed: false,
+      content: '',
+      due: new Date(),
+    })
+
+    return {
+      type: 'success',
+      message: 'Todo created',
+      data: {
+        id: res.id,
+        completed: res.completed,
+        content: res.content,
+        due: new Date(res.due),
+      } as Todo,
+    }
+  } catch {
+    return {
+      type: 'error',
+      message: 'Failed to create todo',
+    }
+  }
+}
+
+export const retrieveTodos = async () => {
+  try {
+    const res = await pb.collection(TODOS).getFullList<Todo>()
+    return {
+      type: 'success',
+      message: 'Todos fetched',
+      data: res.map((todo) => ({
+        id: todo.id,
+        completed: todo.completed,
+        content: todo.content,
+        due: new Date(todo.due),
+      })),
+    }
+  } catch {
+    return {
+      type: 'error',
+      message: 'Failed to get todos',
+    }
+  }
+}
+
+export const updateTodo = async (id: string, todo: Todo) => {
+  try {
+    // Check if the date is valid
+    if (!todo.due || isNaN(todo.due.getTime())) {
+      throw new Error('Invalid date')
+    }
+
+    await pb.collection(TODOS).update(id, {
+      completed: todo.completed,
+      content: todo.content,
+      due: todo.due,
+    })
+    return {
+      type: 'success',
+      message: 'Todo updated',
+    }
+  } catch {
+    return {
+      type: 'error',
+      message: 'Failed to update todo',
+    }
+  }
+}
+
+export const deleteTodo = async (id: string) => {
+  try {
+    await pb.collection(TODOS).delete(id)
+    return {
+      type: 'success',
+      message: 'Todo deleted',
+    }
+  } catch {
+    return {
+      type: 'error',
+      message: 'Failed to delete todo',
+    }
+  }
+}
